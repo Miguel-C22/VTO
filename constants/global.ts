f7a8d3d31866689dbc99f39e8110b6d84e529bd1
Miguel-C22 <miguel.ganoza@treez.io>
2025-03-29 19:28:39 -0500

    updated base url

--- constants/global.ts
+++ constants/global.ts
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:3000"
+// export const BASE_URL = "http://localhost:3000"
+export const BASE_URL = "https://vto-virtual.vercel.app"
 
 export const MANAGER = "manager"
 export const ASSOCIATES = "associate"
